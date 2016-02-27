03bfe1279b0ce2d42a247c1f8f33b56ee0c2ae27
kepaxefel <kepaxefel@mail.ru>
2016-02-27 22:57:00 +0200

    added js

---
+++ public/js/my.js
@@ -0,0 +1,22 @@
+﻿  	show("JavaScript"); // JavaScriptundefined
+
+  	function show(from, to) {
+  	  (to == null) ? alert (from + " - ECMA") : alert (from + to);		  
+	}
+
+	
+	var someone = prompt('Кто пришел?', '');
+	checkSomeone (someone);
+
+	function checkSomeone (someone){
+		if (someone == '') alert('Вход отменен!');
+		if (someone == 'PHP') alert('Нет, не стоит');
+		if (someone == 'JavaScript'){
+			var pass = prompt ('Введите пароль','');
+			if (pass == null) alert ('Вход отменен');
+			if (pass == 'Не важно что') alert ('Пароль не верен');
+			if (pass == 'ECMA') alert ('Добро пожаловать!');
+		}
+	}
+	
+	
